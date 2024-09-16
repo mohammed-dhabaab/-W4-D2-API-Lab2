df0562b0c0e4f913488dceb71c030027f2c60e92
mohammed-dhabaab <mohammed.dhabaab@gmail.com>
2024-09-16 14:26:02 +0300

    Completed

---
+++ main.js
@@ -0,0 +1,7 @@
+
+// const mainContainer = document.querySelector("main-container")
+// const apiURL = ``
+// async function fetchData() {
+//     const respone = await fetch(apiURL)
+//     if
+// }
